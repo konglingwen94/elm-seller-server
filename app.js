6a4734809ef67a7e90c26cf614d7377d64163c93
konglingwen94 <794430774@qq.com>
2020-10-14 18:25:02 +0800

    添加静态资源托管服务

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 const Koa = require("koa");
+const static = require("koa-static");
 const bodyparser = require("koa-bodyparser");
 const connectDB = require("./helper/mongoose");
 const middleware = require("./helper/middleware");
 const router = require("./helper/router");
 
- 
-
 const app = new Koa();
 
-
- 
-
+app.use(static('./public/web'));
 app.use(bodyparser());
 app.use(middleware.response());
-app.use( router.routes());
+app.use(router.routes());
 
 connectDB().then(() => {
   if (process.env.NODE_ENV !== "production") {
     console.clear();
   }
-  app.listen(8888, () => {
-    console.log("Server is listening on http://localhost:8888");
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
   });
 });
