9984ef67327c43b4349c5b4836066cf0e07af7be
konglingwen94 <794430774@qq.com>
2020-11-16 14:45:37 +0800

    添加分页数据查询

--- controller/rating.js
+++ controller/rating.js
@@ -1,12 +1,19 @@
 const RatingModel = require("../model/rating");
+const { resolvePagination, resolveFilterOptions } = require("../helper/utils");
 
 module.exports = {
   async queryList(ctx) {
-    ctx.body=await RatingModel.find();
+    const pagination = resolvePagination({ page: ctx.query.page, pageSize: ctx.query.pageSize });
+    const opts = resolveFilterOptions(pagination);
+    const total = await RatingModel.countDocuments();
+
+    const data = await RatingModel.find().sort(opts.sort).skip(opts.skip).limit(opts.limit);
+    // debugger;
+    ctx.body = { data, pagination, total };
   },
   async deleteOne(ctx) {
     const { id } = ctx.params;
     await RatingModel.findByIdAndDelete(id);
-    ctx.status=204
+    ctx.status = 204;
   },
 };
