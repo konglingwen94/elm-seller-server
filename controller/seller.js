1db2fc0137bd68a6ff6fddbb191381a59b7704da
konglingwen94 <794430774@qq.com>
2020-12-07 13:46:34 +0800

    添加店铺统计数据字段

--- controller/seller.js
+++ controller/seller.js
@@ -1,7 +1,24 @@
 const SellerModel = require("../model/seller");
-// const defaults = require("../helper/utils").defaults;
+const FoodModel = require("../model/foods");
 module.exports = {
   async queryOne(ctx) {
-    ctx.body = await SellerModel.findOne();
+    const totalFoods = await FoodModel.find();
+
+    if (!totalFoods) {
+      ctx.status = 400;
+      return (ctx.body = { message: "没有商铺信息" });
+    }
+
+    const totalPrice = totalFoods.reduce((prev, cur) => {
+      return cur.price * cur.sellCount + prev;
+    }, 0);
+    const totalSellCount = totalFoods.reduce((prev, cur) => {
+      return cur.sellCount + prev;
+    }, 0);
+
+    const totalFoodCount = await FoodModel.countDocuments();
+    
+    const sellerInfo = (await SellerModel.findOne()).toObject();
+    ctx.body = Object.assign(sellerInfo, { totalSellCount, totalPrice, totalFoodCount });
   },
 };
