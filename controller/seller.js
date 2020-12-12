8cd5469653a94923ffdd369d273af3bfea10da7d
konglingwen94 <794430774@qq.com>
2020-12-12 15:21:56 +0800

    优化获取店铺数据接口

--- controller/seller.js
+++ controller/seller.js
@@ -4,10 +4,7 @@ module.exports = {
   async queryOne(ctx) {
     const totalFoods = await FoodModel.find();
 
-    if (!totalFoods) {
-      ctx.status = 400;
-      return (ctx.body = { message: "没有商铺信息" });
-    }
+     
 
     const totalPrice = totalFoods.reduce((prev, cur) => {
       return cur.price * cur.sellCount + prev;
@@ -18,15 +15,24 @@ module.exports = {
 
     const totalFoodCount = await FoodModel.countDocuments();
 
-    const sellerInfo = (await SellerModel.findOne()).toObject();
+    const sellerResult = await SellerModel.findOne();
+
+    if (!sellerResult) {
+      ctx.status = 400;
+      ctx.body = { message: "数据没有初始化" };
+      return;
+    }
+
+    const sellerInfo = sellerResult && sellerResult.toObject();
+
     ctx.body = Object.assign(sellerInfo, { totalSellCount, totalPrice, totalFoodCount });
   },
   async updateOne(ctx) {
     const { bulletin, deliveryPrice, infos, minPrice, name, pics, supports } = ctx.request.body;
 
     const payload = { bulletin, deliveryPrice, infos, minPrice, name, pics, supports };
-    const id=ctx.params.id
+    const id = ctx.params.id;
     await SellerModel.findByIdAndUpdate(id, payload);
-    ctx.status=204
+    ctx.status = 204;
   },
 };
