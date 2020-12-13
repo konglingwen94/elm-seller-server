b9f918779f3b2655720d29cd4410bbbb716ff13a
konglingwen94 <794430774@qq.com>
2020-12-13 17:34:36 +0800

    修复店铺统计计算值为NaN的问题

--- controller/seller.js
+++ controller/seller.js
@@ -4,15 +4,15 @@ module.exports = {
   async queryOne(ctx) {
     const totalFoods = await FoodModel.find();
 
-     
-
     const totalPrice = totalFoods.reduce((prev, cur) => {
-      return cur.price * cur.sellCount + prev;
+      let amount = cur.price * cur.sellCount;
+      amount = Number.isNaN(amount) ? 0 : amount;
+      return amount + prev;
     }, 0);
+     
     const totalSellCount = totalFoods.reduce((prev, cur) => {
-      return cur.sellCount + prev;
+      return prev + (cur.sellCount || 0);
     }, 0);
-
     const totalFoodCount = await FoodModel.countDocuments();
 
     const sellerResult = await SellerModel.findOne();
@@ -28,9 +28,9 @@ module.exports = {
     ctx.body = Object.assign(sellerInfo, { totalSellCount, totalPrice, totalFoodCount });
   },
   async updateOne(ctx) {
-    const { bulletin, deliveryPrice, infos, minPrice, name, pics, supports,avatar } = ctx.request.body;
+    const { bulletin, deliveryPrice, infos, minPrice, name, pics, supports, avatar } = ctx.request.body;
 
-    const payload = {avatar, bulletin, deliveryPrice, infos, minPrice, name, pics, supports };
+    const payload = { avatar, bulletin, deliveryPrice, infos, minPrice, name, pics, supports };
     const id = ctx.params.id;
     await SellerModel.findByIdAndUpdate(id, payload);
     ctx.status = 204;
