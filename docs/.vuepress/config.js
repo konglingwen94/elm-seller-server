7d9253178c49a724a5232d11cfcaf0587f0bd060
konglingwen94 <794430774@qq.com>
2020-12-17 19:53:18 +0800

    搭建文档首页和基本配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const fs = require("fs");
+const _ = require("lodash");
+
+const config = {
+  dest: path.resolve(__dirname, "../../public/docs"),
+
+  base: "/elm-seller-server/public/docs/",
+  title: "elm-seller-server", 
+  themeConfig: {
+    nav: [
+      {
+        text: "API",
+        link: "/api/",
+      },
+      {
+        text: "DATABASE",
+        link: "/database/",
+      },
+      {
+        text: "CONFIG",
+        link: "/configurations",
+      },
+      {
+        text: "GitHub",
+        link: "https://github.com/konglingwen94/elm-seller-server",
+      },
+    ],
+    sidebar: {},
+  },
+};
+
+const dir = path.resolve(__dirname, "../");
+
+const ingored = [".vuepress", "README.md"];
+const dirList = fs.readdirSync(dir).filter((item) => !_.includes(ingored, item));
+
+_.forEach(dirList, (item) => {
+  if (/\.\w+$/g.test(item)) {
+    return;
+  }
+
+  const files = fs.readdirSync(path.resolve(dir, item));
+
+  const arr = _.map(files, (item) => (item === "README.md" ? "" : item));
+
+  config.themeConfig.sidebar[`/${item}/`] = arr;
+});
+
+module.exports = config;
---
+++ docs/configurations.md
@@ -0,0 +1,69 @@
+---
+sidebar: auto
+---
+
+# 项目配置
+
+## 数据库环境
+
+### MongoDB
+
+- [官方文档](https://docs.mongodb.com/manual/introduction/)
+- [安装手册](https://docs.mongodb.com/manual/administration/install-on-linux/)
+
+## 服务端环境
+
+项目服务端基于 [nodejs.org](https://nodejs.org/) 进行开发。
+
+### 版本要求
+
+```
+>= 10.x
+```
+
+### API 服务部署
+
+#### 1. 安装项目依赖
+
+```bash
+npm install
+```
+
+#### 2. 编写配置文件
+
+```js
+// 开发环境配置文件 config/config.local.json
+
+{
+  secretKey: '随机安全 key',
+expiresIn:'token过期时间',  
+  // MongoDB configs.
+  mongodb: {
+    host: '127.0.0.1',
+    port: 27017,
+    database: '数据库名称',
+    username: '数据库账号',
+    password: '数据库密码',
+  },
+}
+```
+
+#### 3. 运行 API 服务
+
+```bash
+npm run start # 启动运行 API 服务
+npm run stop # 停止运行 API 服务
+```
+
+#### 4. 执行初始化数据库脚本
+
+```bash
+# 仅在第一次配置时执行， 默认创建超级管理员ROOT
+node scripts/init-admin.js  管理员用户名 管理员密码
+```
+
+```bash
+# 为了使客户端展示初始化数据，可以在服务启动后一次性初始化编辑好的json数据到数据库.如果不执行此操作，则需要在本项目提供服务的管理后台添加数据才可以  管理后天项目：<https://github.com/konglingwen94/vue-seller-admin>
+
+node scripts/init-database.js
+```
